11d3a7b117d147a015d4fca3198aa66dbc8af745
rrii1 <150480405+rrii1@users.noreply.github.com>
2023-11-13 11:10:50 +0200

    Delete src/main/java/com/example/vii/YourDocumentRepository.java

--- src/main/java/com/example/vii/YourDocumentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.vii;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface YourDocumentRepository extends ElasticsearchRepository<YourDocumentEntity, String> {
-}
